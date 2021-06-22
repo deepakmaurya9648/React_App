777e8ac69a3d9c4050e6632c5e0c1819aee2fc40
Deepak  Maurya <dkmaurya9648@gmail.com>
2021-06-22 11:31:32 +0530

    change the favicon icon

--- avatar_app/src/Avatar.js
+++ avatar_app/src/Avatar.js
@@ -3,18 +3,19 @@ import ReactDom from 'react-dom';
 import './Avatar.css';
 import 'tachyons';
 import AvatarList from './AvatarList';
-
+import Header from './Header';
 const Avatar=()=>{
     let arr=[];
-    for(let i=0;i<1000;i++){
+    for(let i=0;i<100;i++){
         arr.push(i);
     }
     const arrAvatar=arr.map((card,i)=>{
         return <AvatarList id={arr[i]} />
     })
     return (
+
        <div className="tc">
-           <h1 className="tc bg-white-90 pa3">Welcome to avatar world</h1>
+           <Header/>
            {arrAvatar}
        </div>
     )
---
+++ avatar_app/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div style={{width:'100%',background:'#333'}}>
+            <h1 style={{fontSize:"35px",padding:'20px'}}>Avatar App</h1>
+        </div>
+    )
+}
